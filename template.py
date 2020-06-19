b0442c4186279890915938a401cb50b01e8903ea
arturoromeroum <ajruwork@gmail.com>
2020-06-19 12:45:41 -0500

    se modifica el archivo

--- template.py
+++ template.py
@@ -5,8 +5,16 @@ from flask import render_template # libreria que renderiza los templates
 app = Flask(__name__)
 
 @app.route('/')
-def hola_mundo():
+def index():
     return render_template('index.html')
+
+"""en esta ruta le mandamos variables a nuestro template"""
+@app.route('/user/<name>')
+def user(name='Arturo'):
+    age = 19
+    my_list = [1, 2, 3, 4, 5, 6]
+    return render_template('user.html', nombre=name, age=age, list=my_list)
+
 
 if __name__ == '__main__':
     app.run

c1bdb570c9cce5091db9b04f51dcafe9135009a1
arturoromeroum <ajruwork@gmail.com>
2020-06-19 11:58:42 -0500

    se modifica el archivo

--- practica_flask.py
+++ practica_flask.py
@@ -14,5 +14,12 @@ def params():
     param = request.args.get('params1', 'no contiene parametro.')
     return 'el parametro es: {}'.format(param)
 
+# creamos una ruta que recibe un parametro name desde la url
+@app.route('/params/') # en caso de que no se reciba un parametro igual ejecutar params
+@app.route('/params/<name>/<int:num>') # obligamos a que en la ruta sea un int la variable num
+def user(name = 'este es un valor por default', num = 'nada'):
+    """esta funcion recibe el parametro de la url y lo muetra en pantalla"""
+    return 'el parametro es: {} {}'.format(name, num)
+
 if __name__ == '__main__':
     app.run # se encarga de ejecutar el servidor por defecto en el puerto 5000
